b90cb4e456150eae7dac87e969ebb7cf583454d9
ShivamTaneja <shiivamtaneja@gmail.com>
2026-09-14 14:12:14 +0530

    refactor: remove routing, layout components, unused pages, and metadata

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { MemoryRouter, Route, Routes } from "react-router-dom";
-import { Home } from "./pages/home";
-import { NotFound } from "./pages/not-found";
 import "./App.css";
 
 export default function App() {
   return (
-    <MemoryRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </MemoryRouter>
+    <div className="flex min-h-screen flex-col bg-background text-foreground">
+      {/* Desktop app UI goes here */}
+    </div>
   );
 }
