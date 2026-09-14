cbcbd9905101508177e91922ddca0a922ae280af
ShivamTaneja <shiivamtaneja@gmail.com>
2026-09-14 13:20:48 +0530

    chore: replace default template branding with custom Farside assets and metadata

--- src/App.tsx
+++ src/App.tsx
@@ -18,10 +18,10 @@ function App() {
 
       <div className="row">
         <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+          <img src="/logo.svg" className="logo vite" alt="Vite logo" />
         </a>
         <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+          <img src="/logo.svg" className="logo tauri" alt="Tauri logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
