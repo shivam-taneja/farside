5225a3882b8eab2095a725c2b18b467658bf6c76
ShivamTaneja <shiivamtaneja@gmail.com>
2026-09-15 10:28:02 +0530

    feat: integrate shadcn ui with path aliases and a new button component

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import process from "node:process";
+import path from "node:path";
 const host = process.env.TAURI_DEV_HOST;
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig(() => ({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
